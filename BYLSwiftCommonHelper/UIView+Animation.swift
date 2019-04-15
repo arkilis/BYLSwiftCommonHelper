a2c0ff76bdb7947b6e651973383985396d370089
Yang Liu <arkilis@gmail.com>
2019-04-16 06:42:37 +1000

    add Animation fade in and fade out

---
+++ BYLSwiftCommonHelper/UIView+Animation.swift
@@ -0,0 +1,23 @@
+//
+//  UIView+Animation.swift
+//  Australian Slang
+//
+//  Created by Ben Liu on 15/4/19.
+//  Copyright © 2019 Ben Liu. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIView {
+    public func fadeIn(_ duration: TimeInterval = 1.0, delay: TimeInterval = 0.0, completion: @escaping ((Bool) -> Void) = {(finished: Bool) -> Void in}) {
+        UIView.animate(withDuration: duration, delay: delay, options: UIView.AnimationOptions.curveEaseIn, animations: {
+            self.alpha = 1.0
+        }, completion: completion)  }
+    
+    public func fadeOut(_ duration: TimeInterval = 1.0, delay: TimeInterval = 0.0, completion: @escaping (Bool) -> Void = {(finished: Bool) -> Void in}) {
+        UIView.animate(withDuration: duration, delay: delay, options: UIView.AnimationOptions.curveEaseIn, animations: {
+            self.alpha = 0.0
+        }, completion: completion)
+    }
+}
